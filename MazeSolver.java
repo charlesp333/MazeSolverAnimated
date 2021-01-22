417dbe52826c3b6c185d93c7589d8d614fda54b3
charlesp333 <60236224+charlesp333@users.noreply.github.com>
2021-01-22 15:53:29 -0600

    Create MazeSolver.java

---
+++ MazeSolver.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+import java.util.Deque;
+
+
+public class MazeSolver{
+  private Maze maze;
+
+  /**
+   * Constructor for the MazeSolver class.
+   */
+  public MazeSolver(Maze maze){
+    this.maze = maze;
+  }
+
+ 
+  public boolean traverse(){
+    boolean done = false;
+    int row, column;
+    Position pos = new Position();
+    //Deque<Position> stack = new LinkedList<Position>();
+    Frontier<Position> frontier = new MyTree<Position>();
+    //stack.push(pos);
+    frontier.add(pos);
+    
+    while (!(done) && frontier.size() != 0){
+      pos = frontier.remove();
+      if (pos.getx() == maze.getRows()-1 && pos.gety() == maze.getColumns()-1){
+        done = true;  // the maze is solved
+      }else{
+        push_new_pos(pos.getx() - 1,pos.gety(), frontier); 
+        push_new_pos(pos.getx() + 1,pos.gety(), frontier);
+        push_new_pos(pos.getx(),pos.gety() - 1, frontier);
+        push_new_pos(pos.getx(),pos.gety() + 1, frontier); 
+      }
+      System.out.print("\033\143"); 
+      System.out.println(maze);
+      try{
+       Thread.sleep(500);
+      }catch(InterruptedException e){}
+      maze.tryPosition(pos.getx(),pos.gety());  // this cell has been tried
+      //System.out.println(maze);
+    }
+
+    return done;
+  }
+
+
+   public boolean traverse_rc(int row, int column){
+     return false;
+   }
+
+  
+  private void push_new_pos(int x, int y, Frontier<Position> frontier){
+    Position npos = new Position();
+    npos.setx(x);
+    npos.sety(y);
+    int distance = (maze.getRows()-x)+(maze.getColumns() -y);
+    npos.setDistance(distance);
+    if (maze.validPosition(x,y)){
+      frontier.add(npos);
+    }
+  }
+}
